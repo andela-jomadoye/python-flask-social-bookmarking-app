f1c3175a3c648c92aac8c53ca91e8d89580a014c
Omadoye Jedidiah <jedidiah.omadoye@andela.com>
2017-06-27 15:15:21 +0100

    setup MTV

--- __init__.py
+++ __init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/')
 def index():
-    return "This is the index page"
+    return "This is the home page"
 
 if __name__ == "__main__":
     app.run()
